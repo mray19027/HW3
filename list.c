639eb3330f08fd1dd1a429ab53e2731ffdf5719d
Michael Ray <mray190@gmail.com>
2016-01-25 20:36:23 -0500

    Finished problems 4 and 5

--- list.c
+++ list.c
@@ -3,6 +3,11 @@
 
 #include "list.h"
 
+// typedef struct list_t {
+// 	uint32_t index;
+// 	struct list_t *next;
+// } list_t;
+
 // Takes a valid, sorted list starting at `head` and adds the element
 // `new_element` to the list. The list is sorted based on the value of the
 // `index` member in ascending order. Returns a pointer to the head of the list
@@ -11,6 +16,21 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
 
+	list_t* temp = head;
+	if(new_element->index < head->index) {
+		new_element->next = head;
+		return new_element;
+	}
+	while (temp->next!=NULL) {
+		if (new_element->index < temp->next->index) {
+			new_element->next = temp->next;
+			temp->next = new_element;
+			return head;
+		}
+		temp = temp->next;
+	}
+	temp->next = new_element;
+	new_element->next = NULL;
 	return head;
 }
 
@@ -19,6 +39,20 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
 
+	list_t* next_head;
+	list_t* temp;
+	next_head = head->next;
+	int first_run = 1;
+	while(next_head!=NULL) {
+		temp = head;
+		head = next_head;
+		next_head = head->next;
+		head->next = temp;
+		if (first_run) {
+			temp->next = NULL;
+			first_run = 0;
+		}
+	}
 	return head;
 }
 
